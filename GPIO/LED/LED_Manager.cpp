848390e8135819af640426c9003bdfabf81a4ed6
matdombrock <matdombrock@gmail.com>
2023-04-03 02:12:32 -0700

    undo gpio changes

--- GPIO/LED/LED_Manager.cpp
+++ GPIO/LED/LED_Manager.cpp
@@ -7,14 +7,12 @@
 #include <iostream>
 #include <map>
 
-LED led[2] = {LED("gpiochip1", 91), LED("gpiochip1",98)};
+LED led("gpiochip1", 91);// chip name, line number
 
 int main(int argc, char **argv)
 {
   std::cout << "Starting LED Driver Manager" << std::endl;
   std::cout << "===========================" << std::endl;
-  for(int ledIndex = 0; ledIndex < 2; ledIndex++){
-    led[ledIndex].startIPCWatcher("/tmp/led"+std::to_string(ledIndex)+"_fifo");
-  }
+  led.startIPCWatcher("/tmp/led_fifo");
   return 0;
 }
