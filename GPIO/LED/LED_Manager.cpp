9361d15cc5a18789bebafcca9d387a86dd7f8c1a
matdombrock <matdombrock@gmail.com>
2023-04-03 01:51:56 -0700

    multi led

--- GPIO/LED/LED_Manager.cpp
+++ GPIO/LED/LED_Manager.cpp
@@ -1,19 +1,20 @@
 // The LED_Driver IPC Manager
 // IPC via FIFO pipe at /tmp/led_fifo
+// First do
+// $ mkfio /tmp/led_fifo
+
 #include "LED_Driver.h"
 #include <iostream>
+#include <map>
 
-// Config
-const char* chipName = "gpiochip1";
-const int lineNum = 98;
+LED led[2] = {LED("gpiochip1", 91), LED("gpiochip1",98)};
 
 int main(int argc, char **argv)
 {
   std::cout << "Starting LED Driver Manager" << std::endl;
   std::cout << "===========================" << std::endl;
-  LED led(chipName, lineNum);
-  // First do
-  // $ mkfio /tmp/led_fifo
-  led.startIPCWatcher("/tmp/led_fifo");
+  for(int ledIndex = 0; ledIndex > 2; ledIndex++){
+    led[ledIndex].startIPCWatcher("/tmp/led"+std::to_string(ledIndex)+"_fifo");
+  }
   return 0;
 }
