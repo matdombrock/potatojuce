cac07764da1e298bb0219d8020c6286e6f987c83
matdombrock <matdombrock@gmail.com>
2023-04-02 21:51:07 -0700

    led driver

--- GPIO/LED/LED_Manager.cpp
+++ GPIO/LED/LED_Manager.cpp
@@ -1,37 +1,29 @@
-// C++ 11 ONLY
-// This wont work on our OS
 #include <iostream>
-#include <queue>
-#include <mutex>
-#include <thread>
-
-std::queue<std::string> messageQueue;
-std::mutex queueMutex;
-
-void sendMessage(const std::string& message) {
-    std::lock_guard<std::mutex> lock(queueMutex);
-    messageQueue.push(message);
-}
-
-void processMessages() {
-    while (true) {
-        std::lock_guard<std::mutex> lock(queueMutex);
-        while (!messageQueue.empty()) {
-            std::string message = messageQueue.front();
-            messageQueue.pop();
-            // Process the message here
-            std::cout << "Received message: " << message << std::endl;
-        }
-    }
-}
+#include <fstream>
+#include <string>
+#include "LED_Driver.h"
 
 int main() {
-    std::thread messageThread(processMessages);
+    const char* chipName = "gpiochip1";
+    LED led(chipName, 98);
+    led.open();
 
-    // Inject messages into the loop
-    sendMessage("Hello");
-    sendMessage("World");
+    std::string fifo_path = "/tmp/led_fifo"; // replace with your FIFO path
+    std::ifstream fifo(fifo_path);
 
-    messageThread.join();
+    if (!fifo.is_open()) {
+        std::cerr << "Failed to open FIFO: " << fifo_path << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(fifo, line)) {
+        int num;
+        num = std::stoi(line);
+        std::cout << "Received message: " << line << std::endl;
+        led.set((bool)num);
+    }
+    led.close();
+    fifo.close();
     return 0;
 }
