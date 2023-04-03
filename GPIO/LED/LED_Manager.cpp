56ac5ebccd19842b346deb49877dbf5221fd023f
matdombrock <matdombrock@gmail.com>
2023-04-02 21:19:17 -0700

    test fix?

--- GPIO/LED/LED_Manager.cpp
+++ GPIO/LED/LED_Manager.cpp
@@ -2,26 +2,23 @@
 #include <queue>
 #include <mutex>
 #include <thread>
-#include "LED_Driver.h"
 
-std::queue<bool> messageQueue;
+std::queue<std::string> messageQueue;
 std::mutex queueMutex;
 
-void sendMessage(const bool& message) {
+void sendMessage(const std::string& message) {
     std::lock_guard<std::mutex> lock(queueMutex);
     messageQueue.push(message);
 }
 
 void processMessages() {
-    LED led("gpiochip1", 98);
     while (true) {
         std::lock_guard<std::mutex> lock(queueMutex);
         while (!messageQueue.empty()) {
-            bool message = messageQueue.front();
+            std::string message = messageQueue.front();
             messageQueue.pop();
             // Process the message here
             std::cout << "Received message: " << message << std::endl;
-            led.set(message);
         }
     }
 }
@@ -30,11 +27,8 @@ int main() {
     std::thread messageThread(processMessages);
 
     // Inject messages into the loop
-    sendMessage(0);
-    usleep(100000);
-    sendMessage(1);
-    usleep(100000);
-    sendMessage(0);
+    sendMessage("Hello");
+    sendMessage("World");
 
     messageThread.join();
     return 0;
