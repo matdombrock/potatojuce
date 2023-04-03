da8fe3c6b6e2cf792441d3bb10fe2d734135c0be
matdombrock <matdombrock@gmail.com>
2023-04-02 20:04:39 -0700

    update led test

--- GPIO/test_cpp/led.cpp
+++ GPIO/test_cpp/led.cpp
@@ -38,11 +38,13 @@ int main(int argc, char **argv)
 {
   const char* chipName = "gpiochip1";
   LED led(chipName, 98);
+  led.open();
   int i = 0;
   while (true) {
     led.set((i & 1) != 0);
     usleep(100000);
     i++;
   }
+  led.release();
   return 0;
 }
