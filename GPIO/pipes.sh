0b2b957a96738acff177d05c0662bd43fdb91103
matdombrock <matdombrock@gmail.com>
2023-04-06 22:55:40 -0700

    fix pipes

--- GPIO/pipes.sh
+++ GPIO/pipes.sh
@@ -4,15 +4,15 @@ IN=/tmp/pgpio-in
 OUT=/tmp/pgpio-out
 
 echo "Removing Pipes"
-echo IN
-rm IN || exit 1
-echo OUT
-rm OUT || exit 1
+echo $IN
+rm $IN || exit 1
+echo $OUT
+rm $OUT || exit 1
 
 echo "Setting Up Pipes"
-echo IN
-mkfifo IN || exit 1
-echo OUT
-mkfifo OUT || exit 1
+echo $IN
+mkfifo $IN || exit 1
+echo $OUT
+mkfifo $OUT || exit 1
 
 
