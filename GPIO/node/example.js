2808a88827dff51887db0110939f6dd60fc13135
matdombrock <matdombrock@gmail.com>
2023-04-09 02:55:07 -0700

    timeout -> interval

--- GPIO/node/example.js
+++ GPIO/node/example.js
@@ -3,7 +3,7 @@ import GPIO from './index.js';
 const gpio = new GPIO();
 
 let i = 0;
-setTimeout(blink, 500);
+setInterval(blink, 500);
 function blink(){
     const val = i % 1 ? '11' : '00';
     gpio.setAll(val);
