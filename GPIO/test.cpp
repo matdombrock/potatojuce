731307acfbd65883b1fbea2b0c60f2ed6eefd839
matdombrock <matdombrock@gmail.com>
2023-04-09 19:55:41 -0700

    fix pwm

--- GPIO/test.cpp
+++ GPIO/test.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
         std::cout << state << std::endl;
         sleep(1);
     }
-    for(int i = 0; i < 8; i++){
+    for(int i = 1; i < 8; i++){
         led.pwm(i * 10000);
         led2.set(i * 10000);
         std::cout << i << std::endl;
