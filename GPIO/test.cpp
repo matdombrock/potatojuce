bd2683f38658e1a4661ee2d74d8c3d79421019f1
matdombrock <matdombrock@gmail.com>
2023-04-09 19:54:48 -0700

    pwm test

--- GPIO/test.cpp
+++ GPIO/test.cpp
@@ -7,11 +7,17 @@ int main(int argc, char **argv)
     std::cout << "===========================" << std::endl;
     LED led("gpiochip1", 91);
     LED led2("gpiochip1", 98);
-    for(int i = 0; i < 99; i++){
+    for(int i = 0; i < 4; i++){
         bool state = i % 2;
         led.set(state);
         led2.set(!state);
         std::cout << state << std::endl;
+        sleep(1);
+    }
+    for(int i = 0; i < 8; i++){
+        led.pwm(i * 10000);
+        led2.set(i * 10000);
+        std::cout << i << std::endl;
         sleep(1);
     }
     return 0;
